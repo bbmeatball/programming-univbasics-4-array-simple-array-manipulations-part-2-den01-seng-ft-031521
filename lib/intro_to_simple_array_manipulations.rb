7b70307194e7aa576bbfeed92507df034ce54c2e
JoAnn Siciliano <joannsiciliano@gmail.com>
2021-01-29 01:00:20 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,4 +1,6 @@
 def using_concat(array,array)
 my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+more_favs = ["sports cars", "flatiron school"]
 
+my_favorite_things.concat(["sports cars", "flatiron school"])
 end
